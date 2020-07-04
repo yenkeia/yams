fbc66bed7a82ec1df87c59d85314f38d0b09d663
pyq <pyq225@gmail.com>
2020-07-05 03:31:01 +0800

    update

--- game/environ.go
+++ game/environ.go
@@ -7,18 +7,22 @@ import (
 
 var log = golog.New("server")
 
+// Environ 主游戏环境
 type Environ struct {
 	Peer cellnet.GenericPeer
 }
 
+// NewEnviron 初始化
 func NewEnviron() *Environ {
-	return nil
+	return &Environ{}
 }
 
+// Update 更新游戏状态
 func (env *Environ) Update() {
-	log.Debugln("Update")
+	// log.Debugln("Update")
 }
 
+// HandleEvent 处理客户端包
 func (env *Environ) HandleEvent(ev cellnet.Event) {
-	log.Debugln("HandleEvent")
+	// log.Debugln("HandleEvent")
 }
