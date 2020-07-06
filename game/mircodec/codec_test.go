00c5dcf25508affa01ea258dd228cbd09aeb1eb5
pyq <pyq225@gmail.com>
2020-07-07 02:07:20 +0800

    解码客户端包

--- game/mircodec/codec_test.go
+++ game/mircodec/codec_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/yenkeia/yams/game/proto/client"
 	"github.com/yenkeia/yams/game/ut"
 )
 
@@ -26,4 +27,18 @@ func TestCodec(t *testing.T) {
 
 	e.string("123")
 	fmt.Println(buf)
+}
+
+func TestDecodeClientPacket(t *testing.T) {
+	bytes := []byte{
+		// 54, 0, 3, 0,
+		7, 97, 99, 99, 111, 117, 110, 116, 8, 112, 97, 115, 115, 119, 111, 114, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 113, 117, 101, 115, 116, 105, 111, 110, 6, 97, 110, 115, 119, 101, 114, 7, 97, 100, 100, 114, 101, 115, 115}
+	msg := new(client.NewAccount)
+	codec := new(mirCodec)
+	if err := codec.Decode(bytes, msg); err != nil {
+		panic(err)
+	}
+	t.Log(msg.SecretQuestion)
+	t.Log(msg.SecretAnswer)
+	t.Log(msg.EMailAddress)
 }
