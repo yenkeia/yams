7e2e6072ea640d3db8da9ca614313c474d433997
pyq <pyq225@gmail.com>
2020-07-15 19:32:12 +0800

    add useritem

--- assets/miraccount.sql
+++ assets/miraccount.sql
@@ -38,3 +38,44 @@ CREATE TABLE `account_character` (
     `character_id` int(11) DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE `user_item` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `item_id` int(11) DEFAULT NULL,
+    `current_dura` int(11) DEFAULT NULL,
+    `max_dura` int(11) DEFAULT NULL,
+    `count` int(11) DEFAULT NULL,
+    `ac` int(11) DEFAULT NULL,
+    `mac` int(11) DEFAULT NULL,
+    `dc` int(11) DEFAULT NULL,
+    `mc` int(11) DEFAULT NULL,
+    `sc` int(11) DEFAULT NULL,
+    `accuracy` int(11) DEFAULT NULL,
+    `agility` int(11) DEFAULT NULL,
+    `hp` int(11) DEFAULT NULL,
+    `mp` int(11) DEFAULT NULL,
+    `attack_speed` int(11) DEFAULT NULL,
+    `luck` int(11) DEFAULT NULL,
+    `soul_bound_id` int(11) DEFAULT NULL,
+    `bools` int(11) DEFAULT NULL,
+    `strong` int(11) DEFAULT NULL,
+    `magic_resist` int(11) DEFAULT NULL,
+    `poison_resist` int(11) DEFAULT NULL,
+    `health_recovery` int(11) DEFAULT NULL,
+    `mana_recovery` int(11) DEFAULT NULL,
+    `poison_recovery` int(11) DEFAULT NULL,
+    `critical_rate` int(11) DEFAULT NULL,
+    `critical_damage` int(11) DEFAULT NULL,
+    `freezing` int(11) DEFAULT NULL,
+    `poison_attack` int(11) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `character_user_item` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `character_id` int(11) DEFAULT NULL,
+    `user_item_id` int(11) DEFAULT NULL,
+    `type` int(11) DEFAULT NULL,
+    `index` int(11) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- game/item.go
+++ game/item.go
@@ -1,11 +1,18 @@
 package game
 
-import "github.com/yenkeia/yams/game/orm"
+import (
+	"github.com/yenkeia/yams/game/orm"
+)
 
+// item 是数据库里加载出来的 info 的包装
 type item struct {
 	info *orm.ItemInfo
 }
 
 func newItem(ii *orm.ItemInfo) *item {
 	return &item{info: ii}
+}
+
+type userItem struct {
+	*orm.UserItem
 }
