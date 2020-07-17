2c97b238a32a36e4fc7472717315b91b73492070
pyq <pyq225@gmail.com>
2020-07-18 05:33:46 +0800

    update

--- game/aoigrid.go
+++ game/aoigrid.go
@@ -8,7 +8,7 @@ type aoiGrid struct {
 	maxX      int          //格子右边界坐标
 	minY      int          //格子上边界坐标
 	maxY      int          //格子下边界坐标
-	playerIDs map[int]bool //当前格子内的玩家或者物体成员ID
+	objectIDs map[int]bool //当前格子内的玩家或者物体成员ID
 }
 
 //初始化一个格子
@@ -19,31 +19,31 @@ func newAOIGrid(gID, minX, maxX, minY, maxY int) *aoiGrid {
 		maxX:      maxX,
 		minY:      minY,
 		maxY:      maxY,
-		playerIDs: make(map[int]bool),
+		objectIDs: make(map[int]bool),
 	}
 }
 
 //向当前格子中添加一个玩家
-func (g *aoiGrid) add(playerID int) {
-	g.playerIDs[playerID] = true
+func (g *aoiGrid) add(objID int) {
+	g.objectIDs[objID] = true
 }
 
 //从格子中删除一个玩家
-func (g *aoiGrid) remove(playerID int) {
-	delete(g.playerIDs, playerID)
+func (g *aoiGrid) remove(objID int) {
+	delete(g.objectIDs, objID)
 }
 
 //得到当前格子中所有的玩家
-func (g *aoiGrid) getPlyerIDs() (playerIDs []int) {
-	// for k, _ := range g.playerIDs {
-	for k := range g.playerIDs {
-		playerIDs = append(playerIDs, k)
+func (g *aoiGrid) getObjectIDs() (objectIDs []int) {
+	// for k, _ := range g.objectIDs {
+	for k := range g.objectIDs {
+		objectIDs = append(objectIDs, k)
 	}
 	return
 }
 
 //打印信息方法
 func (g *aoiGrid) String() string {
-	return fmt.Sprintf("Grid id: %d, minX:%d, maxX:%d, minY:%d, maxY:%d, playerIDs:%v",
-		g.gID, g.minX, g.maxX, g.minY, g.maxY, g.playerIDs)
+	return fmt.Sprintf("Grid id: %d, minX:%d, maxX:%d, minY:%d, maxY:%d, objectIDs:%v",
+		g.gID, g.minX, g.maxX, g.minY, g.maxY, g.objectIDs)
 }
