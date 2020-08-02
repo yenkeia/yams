ae6e77f3fecdff87a928b361c687871646e0b70e
pyq <pyq225@gmail.com>
2020-08-02 18:33:11 +0800

    update

--- game/monsterbehavior.go
+++ game/monsterbehavior.go
@@ -30,21 +30,33 @@ func newRootNode(m *monster) behavior {
 }
 
 type node struct {
-	status status
+	status   status
+	children []behavior
 }
 
 func (n *node) tick(time.Time) status { return SUCCESS }
 
 // 控制节点 - 选择
-// 选择其子节点中的一个执行
+// 顺序执行所有的子节点，当一个子节点执行结果为 SUCCESS/RUNNING 的时候终止执行并返回 SUCCESS/RUNNING
+// 选择节点可以被理解为一个或门（OR gate）
 type selectNode struct {
 	node
 }
 
+func (n *selectNode) tick(time.Time) status {
+	return SUCCESS
+}
+
 // 控制节点 - 序列
 // 将其所有子节点依次执行，即当前执行的一个子节点返回成功后，再执行下一个子节点
+// 顺序依次执行子节点，如果所有子节点都返回 SUCCESS/RUNNING，则向其父节点返回 SUCCESS/RUNNING
+// 序列节点可以理解为与门（AND gate）
 type sequenceNode struct {
 	node
+}
+
+func (n *sequenceNode) tick(time.Time) status {
+	return SUCCESS
 }
 
 // 控制节点 - 并行
